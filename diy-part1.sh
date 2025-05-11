5b1293c34669be792a3b7cfeb99c211daaa04b29
zfdx123 <2915441170@qq.com>
2025-05-11 08:38:35 +0800

    添加 自动扩容脚本，默认IP改为 192.168.10.1

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+set -ex
+
+sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+
 echo 'src-git mypkg https://github.com/zfdx123/packages.git;packages' >>feeds.conf.default
+
--- diy-part2.sh
+++ diy-part2.sh
@@ -1,1 +1,60 @@
 #!/bin/bash
+
+# 依赖于 parted losetup resize2fs 自动扩容
+cat <<"EOF" >/etc/uci-defaults/70-rootpt-resize
+if [ ! -e /etc/rootpt-resize ] \
+&& type parted > /dev/null \
+&& lock -n /var/lock/root-resize
+then
+ROOT_BLK="$(readlink -f /sys/dev/block/"$(awk -e \
+'$9=="/dev/root"{print $3}' /proc/self/mountinfo)")"
+ROOT_DISK="/dev/$(basename "${ROOT_BLK%/*}")"
+ROOT_PART="${ROOT_BLK##*[^0-9]}"
+parted -f -s "${ROOT_DISK}" \
+resizepart "${ROOT_PART}" 100%
+mount_root done
+touch /etc/rootpt-resize
+ 
+if [ -e /boot/cmdline.txt ]
+then 
+NEW_UUID=`blkid ${ROOT_DISK}p${ROOT_PART} | sed -n 's/.*PARTUUID="\([^"]*\)".*/\1/p'`
+sed -i "s/PARTUUID=[^ ]*/PARTUUID=${NEW_UUID}/" /boot/cmdline.txt
+fi
+ 
+reboot
+fi
+exit 1
+EOF
+
+cat <<"EOF" >/etc/uci-defaults/80-rootfs-resize
+if [ ! -e /etc/rootfs-resize ] \
+&& [ -e /etc/rootpt-resize ] \
+&& type losetup > /dev/null \
+&& type resize2fs > /dev/null \
+&& lock -n /var/lock/root-resize
+then
+ROOT_BLK="$(readlink -f /sys/dev/block/"$(awk -e \
+'$9=="/dev/root"{print $3}' /proc/self/mountinfo)")"
+ROOT_DEV="/dev/${ROOT_BLK##*/}"
+LOOP_DEV="$(awk -e '$5=="/overlay"{print $9}' \
+/proc/self/mountinfo)"
+if [ -z "${LOOP_DEV}" ]
+then
+LOOP_DEV="$(losetup -f)"
+losetup "${LOOP_DEV}" "${ROOT_DEV}"
+fi
+resize2fs -f "${LOOP_DEV}"
+mount_root done
+touch /etc/rootfs-resize
+reboot
+fi
+exit 1
+EOF
+
+chmod +x ./package/base-files/files/etc/uci-defaults/70-rootpt-resize
+chmod +x ./package/base-files/files/etc/uci-defaults/80-rootfs-resize
+
+cat <<"EOF" >>/etc/sysupgrade.conf
+/etc/uci-defaults/70-rootpt-resize
+/etc/uci-defaults/80-rootfs-resize
+EOF
