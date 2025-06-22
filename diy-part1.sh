5c414a5cc98cb7808c6d2b90840d53c1f32cd04a
菜企鹅 <47835976+zfdx123@users.noreply.github.com>
2025-06-22 22:12:48 +0800

    不再修改默认时区

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,7 +4,6 @@ set -ex
 
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/OpenWrt/Zero/g' package/base-files/files/bin/config_generate
-sed -i 's/UTC/Asia\/Shanghai/g' package/base-files/files/bin/config_generate
 sed -i 's/0.openwrt.pool.ntp.org/0.ntp.aliyun.com/g' package/base-files/files/bin/config_generate
 
 echo 'src-git mypkg https://github.com/zfdx123/packages.git;packages' >>feeds.conf.default
